6ea1ce2959f4addb56483e4bbd501d066f40f6aa
Prabakaran T <mailtodecipher@gmail.com>
2026-02-23 12:51:42 +0530

    Update site metadata and add export script
    Update siteUrl and siteRepo to webtamizhan (https://webtamizhan.in / github.com/webtamizhan), replace the Twitter card image, and add an npm "export" script to package.json that sets EXPORT=1 and UNOPTIMIZED=1 before building and running the postbuild script. These changes prepare the site and social preview for deployment under the new domain.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A blog to share my learnings',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://webtamizhan.in',
+  siteRepo: 'https://github.com/webtamizhan/webtamizhan.in',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   email: 'mailtodecipher@gmail.com',
